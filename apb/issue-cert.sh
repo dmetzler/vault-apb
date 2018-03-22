0e2605124284d9af81658c4b81fe456782196297
Michael Hrivnak <mhrivnak@redhat.com>
2018-03-22 14:41:26 -0400

    supports bind parameter

--- apb/issue-cert.sh
+++ apb/issue-cert.sh
@@ -5,15 +5,12 @@ set -x
 echo TESTING
 
 TARGET_NAMESPACE=$1
-
-env | sort
+COMMON_NAME=$2
 
 export VAULT_ADDR=https://vault.$TARGET_NAMESPACE:8200/
 export VAULT_SKIP_VERIFY=true
 export VAULT_TOKEN=$VAULT_TOKEN
-JSON=$(vault write -format=json pki/issue/service common_name="example.com" ttl=72h)
-
-echo "$JSON" > /tmp/cert.json
+JSON=$(vault write -format=json pki/issue/service common_name="$COMMON_NAME" ttl=72h)
 
 echo "$JSON" | jq -r .data.certificate > /tmp/cert.pem
 echo "$JSON" | jq -r .data.private_key > /tmp/cert.key
