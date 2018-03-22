eb359c02138401cdd2902bc64057c9593a740fff
Michael Hrivnak <mhrivnak@redhat.com>
2018-03-22 16:55:13 -0400

    cleanup

--- vault-entry.sh
+++ vault-entry.sh
@@ -2,25 +2,12 @@
 
 set -x
 
-#entrypoint.sh "$@"
-
-#if [[ "$1" != "provision" ]]
-#then
-    #exit 0
-#fi
-
-#sleep 10
-
-#JSON="$3"
-
-#TARGET_NAMESPACE=$(echo "$JSON" | jq -r .namespace)
 TARGET_NAMESPACE=$1
 
 export VAULT_ADDR=https://vault.$TARGET_NAMESPACE.svc:8200/
 export VAULT_CACERT=/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt
 
 INIT_DATA=$(vault init -format=json -key-shares=1 -key-threshold=1)
-#echo $INIT_DATA
 export VAULT_TOKEN=$(echo $INIT_DATA | jq -r .root_token)
 VAULT_UNSEAL_KEY=$(echo $INIT_DATA | jq -r .unseal_keys_hex[0])
 
